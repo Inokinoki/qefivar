aa1f56f4919b1fad2984830a4524293537d7be0c
Inoki <veyx.shaw@gmail.com>
2021-07-03 08:50:10 +0200

    Add tests scaffolder

---
+++ tests/test_parse_boot_name.cc
@@ -0,0 +1,13 @@
+#include <QtTest/QtTest>
+
+class TestParseBootName: public QObject
+{
+    Q_OBJECT
+private slots:
+};
+
+// TODO: Add tests for boot name
+
+QTEST_MAIN(TestParseBootName)
+
+#include "test_parse_boot_name.moc"
---
+++ tests/test_parse_boot_order.cc
@@ -0,0 +1,13 @@
+#include <QtTest/QtTest>
+
+class TestParseBootOrder: public QObject
+{
+    Q_OBJECT
+private slots:
+};
+
+// TODO: Add tests for boot order parsing
+
+QTEST_MAIN(TestParseBootOrder)
+
+#include "test_parse_boot_order.moc"
