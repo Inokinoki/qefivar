cd1386bbf3e77063eb187ac6ddd836e0541573dd
Inoki <veyx.shaw@gmail.com>
2021-07-03 10:57:51 +0200

    Add scaffolder for guid and path test

---
+++ tests/test_parse_boot_guid.cc
@@ -0,0 +1,13 @@
+#include <QtTest/QtTest>
+
+class TestParseBootGuid: public QObject
+{
+    Q_OBJECT
+private slots:
+};
+
+// TODO: Add tests for boot guid
+
+QTEST_MAIN(TestParseBootGuid)
+
+#include "test_parse_boot_guid.moc"
---
+++ tests/test_parse_boot_path.cc
@@ -0,0 +1,13 @@
+#include <QtTest/QtTest>
+
+class TestParseBootPath: public QObject
+{
+    Q_OBJECT
+private slots:
+};
+
+// TODO: Add tests for boot path
+
+QTEST_MAIN(TestParseBootPath)
+
+#include "test_parse_boot_path.moc"
